3e2215bc30875fe2bd9deb34044199f850302ed0
hblanton57 <h.blanton57@gmail.com>
2024-02-12 22:18:14 -0500

    Add tests

--- Middleware.Tests/MyAuthTests.cs
+++ Middleware.Tests/MyAuthTests.cs
@@ -1,16 +1,21 @@
 using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Hosting;
 using Web.Middleware;
 namespace Middleware.Tests;
-public class MyAuthTests
+public class MyAuthTests : IAsyncLifetime
 {
-    [Fact]
-    public async Task MiddlewareTest_FailWhenNotAuthenticated()
+    IHost? host;
+    public Task DisposeAsync()
     {
-        using var host = await new HostBuilder()
+        return Task.CompletedTask;
+    }
+    public async Task InitializeAsync()
+    {
+        host = await new HostBuilder()
             .ConfigureWebHost(webBuilder =>
             {
                 webBuilder
@@ -21,11 +26,40 @@ public class MyAuthTests
                     .Configure(app =>
                     {
                         app.UseMiddleware<Auth>();
+                        app.Run(async context =>
+                        {
+                        await context.Response.WriteAsync("Authenticated!");
+                        });
                     });
             })
             .StartAsync();
+    }
+    [Fact]
+    public async Task MiddlewareTest_FailWhenNotAuthenticated()
+    {
         var response = await host.GetTestClient().GetAsync("/");
         Assert.NotEqual(HttpStatusCode.NotFound, response.StatusCode);
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Failed!", result);
+    }
+    [Fact]
+    public async Task MiddlewareTest_Authenticated()
+    {
+        var response = await host.GetTestClient().GetAsync("/?username=user1&password=password1");
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Authenticated!", result);
+    }
+    [Fact]
+    public async Task MiddlewareTest_FailNoPassword()
+    {
+        var response = await host.GetTestClient().GetAsync("/?username=user1");
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Failed!", result);
+    }
+    [Fact]
+    public async Task MiddlewareTest_FailWrongUserNameAndPassword()
+    {
+        var response = await host.GetTestClient().GetAsync("/?username=user5&password=password2");
         var result = await response.Content.ReadAsStringAsync();
         Assert.Equal("Failed!", result);
     }
